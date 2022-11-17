347ffcd83876d2962d62f3a2e5a7bd6010d0aa37
daydin14 <david.aydin6@gmail.com>
2022-11-16 19:09:27 -0500

    db config, db connection test, app listener

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
+// Dependencies
+
 require("dotenv").config()
 const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const PORT = process.env.PORT;
 const app = express();
+
+// Database Configuration
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+
+// Database Connection Error / Success
+
+db.on("error", (err) => console.log(err.message + "is mongod not running?"));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
+
+// App Listener
+
+app.listen(PORT, () => console.log(`express is listening on port : ${PORT}`));
